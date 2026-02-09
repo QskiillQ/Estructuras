8dc7906828bcde561a64b3d500271226dbc7f7a6
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:10:24 -0500

    se cambio el hola mundo por una calculadora de suma de dos numeros

--- nacholee.java
+++ nacholee.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class nacholee {
     public static void main(String[] args) {
-        System.out.println("hola mundo ");
+        System.out.println("calculadora de suma");
+        int n1=0, n2=0, suma=0;
+        Scanner leer = new Scanner(System.in);
+        System.out.println("ingrese el primer numero");
+        n1=leer.nextInt();
+        System.out.println("ingrese su segundo numero");
+        n2=leer.nextInt();
+        suma=n1+n2;
+        System.out.println("la suma de los numeros es= "+ suma);
     }
 }
