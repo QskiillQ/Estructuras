32db682ed987cf5812a01e759a3661fe5ebc74f9
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:17:40 -0500

    se agregaron resta multiplicacion y division

--- nacholee.java
+++ nacholee.java
@@ -2,14 +2,31 @@ import java.util.Scanner;
 
 public class nacholee {
     public static void main(String[] args) {
-        System.out.println("calculadora de suma");
-        int n1=0, n2=0, suma=0;
+        System.out.println("calculadora de suma, resta, multiplicacion y division");
+        int n1=0, n2=0;
         Scanner leer = new Scanner(System.in);
         System.out.println("ingrese el primer numero");
         n1=leer.nextInt();
         System.out.println("ingrese su segundo numero");
         n2=leer.nextInt();
-        suma=n1+n2;
-        System.out.println("la suma de los numeros es= "+ suma);
+        System.out.println("la suma de los numeros es= "+ (n1+n2));
+        /*Resta de los dos numeros */
+        System.out.println("ingrese el primer numero");
+        n1=leer.nextInt();
+        System.out.println("ingrese su segundo numero");
+        n2=leer.nextInt();
+        System.out.println("la suma de los numeros es= "+ (n1-n2));
+        /*Multiplicacion */
+        System.out.println("ingrese el primer numero");
+        n1=leer.nextInt();
+        System.out.println("ingrese su segundo numero");
+        n2=leer.nextInt();
+        System.out.println("la suma de los numeros es= "+ (n1*n2));
+        /*Dvision */
+        System.out.println("ingrese el primer numero");
+        n1=leer.nextInt();
+        System.out.println("ingrese su segundo numero");
+        n2=leer.nextInt();
+        System.out.println("la suma de los numeros es= "+ (n1/n2));
     }
 }
